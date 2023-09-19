711ea11a52067bd6e3262561bb4c1d98b59b136e
sdhanraj300 <sdhanraj300@gmail.com>
2023-09-19 20:17:09 +0530

    third commit

---
+++ netflix-taiwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS component.
+    </div>
+  );
+};
+
+export default App;
